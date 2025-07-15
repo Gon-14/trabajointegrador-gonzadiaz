2aa364c039e5bfbe5b7990e58e1d287ef7a59fbf
gonza <gonchidiazriver@gmail.com>
2025-07-15 00:53:15 -0300

    mejorado

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-let items = []; //vamos a guardar lo que escriben los usuarios en este array
+let items = []; 
 
-const formulario = document.getElementById("formulario")
-const input = document.getElementById("input-texto")
-const lista = document.getElementById("lista")
+const formulario = document.getElementById("formulario");
+const input = document.getElementById("input-texto");
+const lista = document.getElementById("lista");
 
 const mostrarLista = () => {
-    lista.innerHTML = items.map((item) => <div class="tarjeta"> ${item}</div>)
-    .join("")
-}
+    lista.innerHTML = items.map((item) => `<div class="tarjeta">${item}</div>`).join("");
+};
+
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     const texto = input.value.trim();
     if (texto === "") return; 
 
     items.push(texto); 
-    input.value. = ""; 
+    input.value = ""; 
     mostrarLista();
-    
-})
+});
